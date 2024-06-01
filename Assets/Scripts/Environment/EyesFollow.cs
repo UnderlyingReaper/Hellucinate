c758671f0475d66a5bdc2dc49ce53b23bd3200ce
G1tichz <155902197+G1tichz@users.noreply.github.com>
2024-06-01 15:41:47 +0300

    Adding a new area which is interesting imo

---
+++ Assets/Scripts/Environment/EyesFollow.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+
+public class EyesFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float range;
+    public Transform centerEye;
+    public Vector2 centerEyePos;
+
+    public float timeDelay;
+    public float timeRandomness;
+    public float blinkDuration;
+    public Color blinkColor;
+
+
+    SpriteRenderer _spriteRenderer;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        StartCoroutine(Blink());
+    }
+
+    void Update()
+    {
+        if(player == null) return;
+
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if(distance <= range)
+        {
+            centerEye.DOLocalMove(centerEyePos, 0.3f);
+            EyeFollow();
+        }
+        else
+        {
+            centerEye.DOLocalMove(Vector2.zero, 0.3f);
+            centerEye.DORotate(Vector2.zero, 0.3f);
+        }
+        
+    }
+
+    void EyeFollow()
+    {
+        Vector2 playerPos = player.transform.position;
+
+        Vector2 direction = new Vector2(playerPos.x - transform.position.x, playerPos.y - transform.position.y);
+
+        transform.up = direction;
+    }
+
+    IEnumerator Blink()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(timeDelay - timeRandomness, timeDelay + timeRandomness));
+            StartCoroutine(BlinkOnce());
+        }
+    }
+    public IEnumerator BlinkOnce()
+    {
+        DOVirtual.Color(_spriteRenderer.color, blinkColor, blinkDuration, value => { _spriteRenderer.color = value; });
+        yield return new WaitForSeconds(blinkDuration);
+        DOVirtual.Color(_spriteRenderer.color, Color.red, blinkDuration, value => { _spriteRenderer.color = value; });
+    }
+}
